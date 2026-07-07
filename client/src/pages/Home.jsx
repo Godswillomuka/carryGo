f429edae945c6a5d700f131bef0d503928bdfecf
Godswill omuka <omukagodswil@gmail.com>
2026-07-07 14:18:39 +0300

    install frontend required dependencies

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import ContactNav from "../components/contactNav";
+
+function Home() {
+  return (
+    <>
+    <ContactNav />
+    <div>
+      
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple home page component.</p>
+    </div>
+    </>
+  );
+}
+
+export default Home;
